8967c576a689740736a2f4f3f27154459e8d4025
Hanna Lee <8annahxxl@gmail.com>
2021-07-01 18:13:12 +0900

    Add : 회원가입, 로그인 유효성 검사 추가 #2

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,13 +1,40 @@
 import express from "express";
 import "express-async-errors";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.js";
 import * as userCtrl from "../controller/userController.js";
 import { isAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/join", userCtrl.join);
+const validateLogin = [
+  body("username")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("아이디는 3자 이상이어야 합니다."),
+  body("password").trim().notEmpty().withMessage("비밀번호를 입력해 주세요."),
+  validate,
+];
 
-router.post("/login", userCtrl.login);
+const validateJoin = [
+  ...validateLogin,
+  body("name").trim().notEmpty().withMessage("이름을 입력해 주세요."),
+  body("email")
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("이메일 형식이 올바르지 않습니다."),
+  body("url")
+    .isURL()
+    .withMessage("유효하지 않은 URL입니다.")
+    .optional({ nullable: true, checkFalsy: true }),
+  validate,
+];
+
+router.post("/join", validateJoin, userCtrl.join);
+
+router.post("/login", validateLogin, userCtrl.login);
 
 router.get("/me", isAuth, userCtrl.me);
 
