b97939ccbfaf8177cbe552892aad491bbfd4dcdd
Hanna Lee <8annahxxl@gmail.com>
2021-07-04 20:53:28 +0900

    Add : 댓글 테이블 기본 쿼리 작성 #5

--- src/data/comments.js
+++ src/data/comments.js
@@ -1,68 +1,63 @@
-import * as userRepo from "./users.js";
+import { db } from "../db.js";
 
-let comments = [
-  {
-    id: "3",
-    content: "첫번째 게시글의 댓글22222",
-    createdAt: new Date().toString(),
-    postId: "1",
-    userId: "1",
-  },
-  {
-    id: "2",
-    content: "댓글22222",
-    createdAt: new Date().toString(),
-    postId: "2",
-    userId: "1",
-  },
-  {
-    id: "1",
-    content: "댓글11111",
-    createdAt: new Date().toString(),
-    postId: "1",
-    userId: "1",
-  },
-];
+// let comments = [
+//   {
+//     id: "3",
+//     content: "첫번째 게시글의 댓글22222",
+//     createdAt: new Date().toString(),
+//     postId: "1",
+//     userId: "1",
+//   },
+//   {
+//     id: "2",
+//     content: "댓글22222",
+//     createdAt: new Date().toString(),
+//     postId: "2",
+//     userId: "1",
+//   },
+//   {
+//     id: "1",
+//     content: "댓글11111",
+//     createdAt: new Date().toString(),
+//     postId: "1",
+//     userId: "1",
+//   },
+// ];
 
-export async function getById(commentId) {
-  const comment = comments.find((comment) => comment.id === commentId);
-  const { username, name, url } = await userRepo.findById(comment.userId);
-  return {
-    ...comment,
-    username,
-    name,
-    url,
-  };
-}
+const SELECT_JOIN =
+  "SELECT comments.id, comments.content, comments.createdAt, comments.postId, comments.userId, users.username, users.name, users.url FROM comments JOIN users ON comments.userId=users.id";
 
 // 해당 게시글의 모든 댓글 가져오기
 export async function getAllByPostId(postId) {
-  let foundComments = comments.filter((comment) => comment.postId === postId);
-  return Promise.all(
-    foundComments.map(async (comment) => await getById(comment.id))
-  );
+  return db
+    .execute(`${SELECT_JOIN} WHERE postId=? ${ORDER_DESC}`, [postId]) //
+    .then((result) => result[0]);
+}
+
+export async function getById(commentId) {
+  return db
+    .execute(
+      `${SELECT_JOIN} WHERE comments.id=? ORDER BY comments.createdAt DESC`,
+      [commentId]
+    ) //
+    .then((result) => result[0][0]);
 }
 
 export async function create(content, userId, postId) {
-  const newComment = {
-    id: Date.now().toString(),
-    content,
-    createdAt: new Date().toString(),
-    postId,
-    userId,
-  };
-  comments.unshift(newComment);
-  return getById(newComment.id);
+  return db
+    .execute(
+      "INSERT INTO comments (content, createdAt, postId, userId) VALUES(?,?,?,?)",
+      [content, new Date(), postId, userId]
+    ) //
+    .then((result) => getById(result[0].insertId));
 }
 
 export async function update(commentId, content) {
-  const comment = comments.find((comment) => comment.id === commentId);
-  if (comment) {
-    comment.content = content;
-  }
-  return getById(comment.id);
+  return db
+    .execute("UPDATE comments SET content=? WHERE id=?", [content, commentId]) //
+    .then(() => getById(commentId));
 }
 
 export async function remove(commentId) {
-  comments = comments.filter((comment) => comment.id !== commentId);
+  return db.execute("DELETE FROM comments WHERE id=?", [commentId]);
 }
